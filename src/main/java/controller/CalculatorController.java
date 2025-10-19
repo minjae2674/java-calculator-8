b2cb557739f7afb53301fa7585d448d41671dcd5
CHOMINJAE <chomin0514@naver.com>
2025-10-19 15:15:34 +0900

    feat: Calculator 빈 입력 0반환

--- src/main/java/controller/CalculatorController.java
+++ src/main/java/controller/CalculatorController.java
@@ -1,5 +1,6 @@
 package controller;
 
+import model.Calculator;
 import view.InputView;
 import view.OutputView;
 
--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -1,4 +1,9 @@
 package model;
 
 public class Calculator {
+    public static int add(String input) {
+        if (input == null || input.isBlank()) {
+            return 0;
+        }
+    }
 }
