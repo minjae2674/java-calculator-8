9b2abe7a467288b7f35cacf85798689fe8a5c5f6
CHOMINJAE <chomin0514@naver.com>
2025-10-20 01:20:42 +0900

    feat: 덧셈 계산기 생성

--- src/main/java/calculator/Calculator.java
+++ src/main/java/calculator/Calculator.java
@@ -1,4 +1,12 @@
 package calculator;
 
+import calculator.operate.Operation;
+import calculator.operate.Plus;
+
 public class Calculator {
+    Operation calculator = new Plus();
+
+    public int run(String str) {
+        return calculator.calculate(str);
+    }
 }
