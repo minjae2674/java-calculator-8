a2a839d5ec50c8b7325d35444a250a3c02e0bf08
CHOMINJAE <chomin0514@naver.com>
2025-10-19 13:06:14 +0900

    feat: Application클래스 시작점 구현

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,11 +1,12 @@
 package calculator;
 
 import camp.nextstep.edu.missionutils.Console;
+import controller.CalculatorController;
+import model.Calculator;
 
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
-
-
+        new CalculatorController().run();
     }
 }
