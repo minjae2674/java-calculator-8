0464bf3e0c6c0d4995be6e866cea0a7d067003e4
CHOMINJAE <chomin0514@naver.com>
2025-10-19 15:40:12 +0900

    feat: 각 토큰 예외처리 후 정수로 변환 후 합산

--- src/main/java/model/Calculator.java
+++ src/main/java/model/Calculator.java
@@ -18,5 +18,23 @@ public class Calculator {
 
         String[] tokens = numbers.split(delimiter);
 
+        int sum = 0;
+        for (String token : tokens) {
+            sum += parsePositiveNumber(token);
+        }
+
+        return sum;
+    }
+
+    private static int parsePositiveNumber(String str) {
+        try {
+            int num = Integer.parseInt(str);
+            if (num < 0) {
+                throw new IllegalArgumentException("음수는 허용되지 않습니다: " + num);
+            }
+            return num;
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException("숫자가 아닌 값이 포함되어 있습니다: " + str);
+        }
     }
 }
